7f196d940b499edfb65a49ce9887b9ac833bbb4d
Pauline Riedinger <levron.pauline@gmail.com>
2024-11-14 16:26:57 +0100

    split code

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+function Footer() {
+  return (
+    <footer>
+      <p>
+        Made with <a href="https://fr.react.dev/">React</a> at{" "}
+        <a href="https://www.lereacteur.io/">Le Reacteur</a> by{" "}
+        <a href="https://github.com/PaulineRiedinger">Pauline</a>
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,10 +1,61 @@
-<form onSubmit={handleSubmit}>
-  <span>Name</span>
-  <input type="text" placheholder="Jean Dupont" />
-  <span>Email</span>
-  <input type="email" placheholder="jeandupont@lereacteur.io" />
-  <span>Password</span>
-  <input type="password" placheholder="lErEactEuR2024" />
-  <span>Confirm your password</span>
-  <input type="password" placheholder="lErEactEuR2024" />
-</form>;
+import { useState } from "react";
+
+function Form() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (password !== confirmPassword) {
+      alert("Les mots de passe ne sont pas identiques");
+    } else {
+      // Logique d'enregistrement si les mots de passe correspondent
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>Name</label>
+        <input
+          type="text"
+          placeholder="Jean Dupont"
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+        />
+      </div>
+      <div>
+        <label>Email</label>
+        <input
+          type="email"
+          placeholder="jeandupont@lereacteur.io"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+      </div>
+      <div>
+        <label>Password</label>
+        <input
+          type="password"
+          placeholder="lErEactEuR2024"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+      </div>
+      <div>
+        <label>Confirm your password</label>
+        <input
+          type="password"
+          placeholder="lErEactEuR2024"
+          value={confirmPassword}
+          onChange={(event) => setConfirmPassword(event.target.value)}
+        />
+      </div>
+      <button type="submit">Register</button>
+    </form>
+  );
+}
+
+export default Form;
